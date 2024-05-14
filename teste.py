099d9f603e9c2fc3f17a890333bfa6214d73048a
Mateus Robers <robersmateus@gmail.com>
2024-05-14 14:28:33 -0300

    a

--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 import telebot
 from telebot import types
 import pandas as pd
+import csv
 
 chave_api = '6408775461:AAFIhWk40_iqu2prkLSpVUn0xwl1Yas5MXI'
 bot = telebot.TeleBot(chave_api)
@@ -46,11 +47,212 @@ def opcao1(callback):
             bot.send_message(callback.message.chat.id, 'Que tipo de filme gostaria de ver hoje?', reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call:True)
+def resposta(callback):
+    if callback.message:
+        if callback.data == 'action':
+            filmes_acao = pd.read_csv('filmes.csv')
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de ação...')
+            for linha in filmes_acao:
+                if linha[4] == 'action':
+                    print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'adventure':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de aventura...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'adventure':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+            
+        elif callback.data == 'animation':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de animação...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'animation':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'anime':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de anime...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'anime':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'biography':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de biografias...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'biography':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'comedy':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de comédia...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'comedy':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'crime':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de crime...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'crime':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+                    
+        elif callback.data == 'documentary':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de documentários...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'documentary':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+                    
+        elif callback.data == 'drama':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de dramas...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'drama':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'entertainment':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de entretenimento...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'entertainment':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'fantasy':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de fantasia...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'fantasy':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'history':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de história...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'history':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'holiday':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes para feriados...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'holiday':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'horror':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de terror...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'horror':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'music':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de música...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'music':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'musical':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de musicais...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'musical':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'nature':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de natureza...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'nature':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'news':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de noticiários...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'news':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'reality':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de reality-shows...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'reality':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'romance':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de romance...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'romance':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'short':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de curta metragens...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'short':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'sports':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de esporte...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'sports':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'variety':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes variados...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'variety':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'war':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de guerra...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'war':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+        elif callback.data == 'western':
+            bot.send_message(callback.message.chat.id, 'Trazendo uma lista de filmes de velho-oeste...')
+            with open("/Users/rober/Desktop/Estudos/filmes.csv", mode='r') as file:
+                leitor_csv = csv.reader(file)
+                for linha in leitor_csv:
+                    if linha[4] == 'western':
+                        print(f"Título: {linha[0]}\nData de lançamento: {linha[1]}\nClassificação do Público: {linha[2]}\nClassificação da Crítica: {linha[3]}")
+
+@bot.callback_query_handler(func=lambda call:True)
 def opcao2(callback):
     if callback.message:
         if callback.data == 'n':
-            texto = 'Ok então, nos vemos outra hora.'
-            bot.send_message(callback.message.chat.id, texto)
+            bot.send_message(callback.message.chat.id, 'Ok então, nos vemos outra hora.')
     
 def verificar(mensagem):
     return True
